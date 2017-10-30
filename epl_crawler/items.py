f859245983ef684b5d3de61e62b425ddf84be8a6
jianmyphy123 <jianmyphy123@tutanota.com>
2017-10-31 02:02:19 +1100

    define Item Class

--- epl_crawler/items.py
+++ epl_crawler/items.py
@@ -8,7 +8,14 @@
 import scrapy
 
 
-class EplCrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class EPLItem(scrapy.Item):
+    club_name = scrapy.Field()
+    position = scrapy.Field()
+    played = scrapy.Field()
+    won = scrapy.Field()
+    drawn = scrapy.Field()
+    lost = scrapy.Field()
+    gf = scrapy.Field()
+    ga = scrapy.Field()
+    gd = scrapy.Field()
+    points = scrapy.Field()
